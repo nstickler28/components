02dea8a6bba5ee082db0e4adbb4b58a5a7c6ef65
nstickler28 <nathanstickler@gmail.com>
2022-06-14 21:35:42 -0700

    Wrap all comment details inside approval cards

--- src/index.js
+++ src/index.js
@@ -7,25 +7,37 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail
-                author="Nate"
-                time="Today at 4:40PM"
-                comment="Hey"
-                avatar={faker.image.image()}
-            />
-            <CommentDetail
-                author="Alex"
-                time="Today at 8:30PM"
-                comment="Yooo"
-                avatar={faker.image.image()}
-            />
-            <CommentDetail
-                author="Brendan"
-                time="Yesterday at 2:00PM"
-                comment="How's it goin?"
-                avatar={faker.image.image()}
-            />
+            <ApprovalCard >
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard >
+                <CommentDetail
+                    author="Nate"
+                    time="Today at 4:40PM"
+                    comment="Hey"
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail
+                    author="Alex"
+                    time="Today at 8:30PM"
+                    comment="Yooo"
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail
+                    author="Brendan"
+                    time="Yesterday at 2:00PM"
+                    comment="How's it goin?"
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
