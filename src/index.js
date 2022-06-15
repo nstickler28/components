681369087b06fdb0accb08131f901ee80c94c5cd
nstickler28 <nathanstickler@gmail.com>
2022-06-14 20:38:55 -0700

    Passed more properties between parent and child

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Nate" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Brendan" />
+            <CommentDetail
+                author="Nate"
+                time="Today at 4:40PM"
+                comment="Hey"
+                avatar={faker.image.image()}
+            />
+            <CommentDetail
+                author="Alex"
+                time="Today at 8:30PM"
+                comment="Yooo"
+                avatar={faker.image.image()}
+            />
+            <CommentDetail
+                author="Brendan"
+                time="Yesterday at 2:00PM"
+                comment="How's it goin?"
+                avatar={faker.image.image()}
+            />
         </div>
     );
 };
